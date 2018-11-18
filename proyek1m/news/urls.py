c7acfeefc9eec880366466cefd4983c32007d602
oon arfiandwi <oon.arfiandwi@gmail.com>
2018-11-18 18:06:21 +0800

    mengaktifkan aplikasi news dengan menambahkan di settings.py, menambahkan form di aplikasi news, menampilkan form dengan template name.html, menangani form submit dengan method POST di views, menampilkan kembali data yang disubmit menggunakan fitur django messages. http://127.0.0.1:8000/news/your-name/

--- proyek1m/news/urls.py
+++ proyek1m/news/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('your-name/', views.get_name),
+    path('thanks/', views.get_name),
     path('', views.index),
 ]
--- proyek1m/news/views.py
+++ proyek1m/news/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt  # THIS IS TEMPORARY, to allow form POST
+
+from .forms import NameForm
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse('Hello, World!')
+
+
+@csrf_exempt  # this is temporary, to allow form POST, NOT SECURE, remove this later
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            messages.info(request, 'hello {}'.format(form.cleaned_data['your_name']))
+            return HttpResponseRedirect('/news/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
